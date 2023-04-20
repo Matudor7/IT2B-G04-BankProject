2cd6b43923cdb6857b1854d2c016e016f11a6c98
Tudor <tdr.nsk@gmail.com>
2023-04-20 14:10:43 +0200

    added springboot framework

---
+++ IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/It2bankApplication.java
@@ -0,0 +1,13 @@
+package nl.inholland.it2bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It2bankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(It2bankApplication.class, args);
+	}
+
+}
---
+++ IT2B-G05-BankApp/it2bank/src/test/java/nl/inholland/it2bank/It2bankApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.inholland.it2bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It2bankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
