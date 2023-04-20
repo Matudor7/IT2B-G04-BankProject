f768ac1f2da85e21c7b7497068e10ef6f03c2545
Tudor <tdr.nsk@gmail.com>
2023-04-20 14:15:22 +0200

    added lombok

--- IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/It2bankApplication.java
+++ IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/It2bankApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class It2bankApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(It2bankApplication.class, args);
 	}
