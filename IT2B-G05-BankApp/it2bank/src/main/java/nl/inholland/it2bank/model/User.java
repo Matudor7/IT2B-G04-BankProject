7470ce45e9b1d62ea4a4c7ab7cb264947c97b3a3
Matudor7 <tdr.nsk@gmail.com>
2023-05-14 16:09:14 +0200

    finished User model. added jakarta persistence dependency

--- IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/model/User.java
+++ IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/model/User.java
@@ -1,5 +1,6 @@
 package nl.inholland.it2bank.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,13 +8,76 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String firstName;
     private String lastName;
     private Long bsn;
     private Integer phoneNumber;
     private String email;
+    private String password;
+
+    @ManyToOne //User has only one role, whilst a role can belong to multiple users
     private UserRoles role;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Long getBsn() {
+        return bsn;
+    }
+
+    public void setBsn(Long bsn) {
+        this.bsn = bsn;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRoles getRole() {
+        return role;
+    }
+
+    public void setRole(UserRoles role) {
+        this.role = role;
+    }
 }
