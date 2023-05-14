eb34c86222350bcdb9bcb3f2ce8e0b3306c01222
Catalin246 <avorniceseicatalin1234@gmail.com>
2023-05-14 16:46:58 +0200

    transaction model

--- IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/model/Transaction.java
+++ IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/model/Transaction.java
@@ -1,4 +1,30 @@
 package nl.inholland.it2bank.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private double amount;
+
+    private Time time;
+
+    private String comment;
 }
