75f157743582a50ce130834ae7a5c79a66d6d5d5
Tudor <tdr.nsk@gmail.com>
2023-05-16 13:58:26 +0200

    create user works now

--- IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/controller/UserController.java
+++ IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/users",
-        method = {RequestMethod.POST})
+        method = {RequestMethod.PUT})
 @Log
 public class UserController {
 
--- IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/model/dto/UserDTO.java
+++ IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/model/dto/UserDTO.java
@@ -1,6 +1,4 @@
 package nl.inholland.it2bank.model.dto;
 
-import nl.inholland.it2bank.model.UserRoles;
-
 public record UserDTO(String firstName, String lastName, long bsn, String phoneNumber, String email, String password, int roleId) {
 }
