e1921deae5b669dee495b4c06300dce45b55b9b5
Catalin246 <avorniceseicatalin1234@gmail.com>
2023-05-22 14:12:12 +0200

    assign id to account

--- IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/repository/AccountRepository.java
+++ IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/repository/AccountRepository.java
@@ -4,6 +4,9 @@ import nl.inholland.it2bank.model.AccountModel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends CrudRepository<AccountModel, Long> {
+    Optional<AccountModel> findByIban(String finalIban);
 }
