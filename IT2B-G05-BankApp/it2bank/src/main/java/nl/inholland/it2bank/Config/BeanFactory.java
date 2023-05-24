ec990bfe6c40063846fd125e59a264d3bf8beb14
RobyParak <677676@student.inholland.nl>
2023-05-24 12:06:26 +0200

    BeanFactory

---
+++ IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/Config/BeanFactory.java
@@ -0,0 +1,21 @@
+package nl.inholland.it2bank.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Random;
+
+@Configuration
+public class BeanFactory {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public Random randomizer() {
+        return new Random();
+    }
+}
