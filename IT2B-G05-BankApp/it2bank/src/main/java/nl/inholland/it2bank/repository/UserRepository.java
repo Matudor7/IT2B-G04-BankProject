4398aa12449009d626c1596605c0ecb77f3e6d4f
Tudor <tdr.nsk@gmail.com>
2023-05-24 11:55:48 +0200

    user repo updated and JWT Token Provider added

--- IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/repository/UserRepository.java
+++ IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/repository/UserRepository.java
@@ -4,6 +4,9 @@ import nl.inholland.it2bank.model.UserModel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<UserModel, Long>{
+    Optional<UserModel> findUserByUsername(String name);
 }
