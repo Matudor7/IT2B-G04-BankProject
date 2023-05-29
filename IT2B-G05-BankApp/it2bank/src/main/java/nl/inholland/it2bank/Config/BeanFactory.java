7258a21bc668a88d1d582b58a7af9e9db89793bd
RobyParak <677676@student.inholland.nl>
2023-05-29 11:42:51 +0200

    Fixed error with passwordEncryption

--- IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/Config/BeanFactory.java
+++ IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/Config/BeanFactory.java
@@ -2,6 +2,7 @@ package nl.inholland.it2bank.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Random;
