c9806fb6a53e43f1a7855329a43ba4bab2560f81
RobyParak <677676@student.inholland.nl>
2023-06-01 14:45:33 +0200

    Added accounts

--- IT2B-G05-BankApp/it2bank/src/main/resources/data.sql
+++ IT2B-G05-BankApp/it2bank/src/main/resources/data.sql
@@ -4,3 +4,15 @@ INSERT INTO user_model(first_name, last_name, bsn, phone_number, email, password
 VALUES ('Revolver', 'Ocelot', 23456789, '0676934748', 'shalashaska_ocelot@gmail.com', 'liquidOcelot4', 0);
 INSERT INTO user_model(first_name, last_name, bsn, phone_number, email, password, role)
 VALUES ('Solid', 'Snake', 34567890, '0612345678', 'solidsnake@gmail.com', 'solidSnake4', 1);
+
+-- This below is the bank itself bank account. DO NOT remove this.
+INSERT INTO user_model(first_name, last_name, bsn, phone_number, email, password, role)
+VALUES ('Bank', 'je', 00000000, '0612345678', 'bankje@gmail.com', 'bankje', 1);
+INSERT INTO account_model (iban, owner_id, status_id, amount, absolute_limit, type_id)
+VALUES ('NL01INHO0000000001', 4, 0, 10000000, 0, 1);
+INSERT INTO account_model (iban, owner_id, status_id, amount, absolute_limit, type_id)
+VALUES ('NL01INHO0000000014', 3, 0, 1000, 0, 2);
+INSERT INTO account_model (iban, owner_id, status_id, amount, absolute_limit, type_id)
+VALUES ('NL01INHO0000000022', 1, 0, 1000, 0, 2);
+INSERT INTO account_model (iban, owner_id, status_id, amount, absolute_limit, type_id)
+VALUES ('NL01INHO0000000030', 2, 0, 1000, 0, 2);
