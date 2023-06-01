04bf9b72082efd96182e6be548af7d2b6b2673bd
myrthe678105 <90244561+myrthe678105@users.noreply.github.com>
2023-06-01 15:19:42 +0200

    added TL & DL

--- IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/model/UserModel.java
+++ IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/model/UserModel.java
@@ -24,7 +24,11 @@ public class UserModel {
     private String password;
     private UserRoles role;
 
-    public UserModel(String firstName, String lastName, long bsn, String phoneNumber, String email, String password, UserRoles role) {
+    private double transactionLimit;
+
+    private double dailyLimit;
+
+    public UserModel(String firstName, String lastName, long bsn, String phoneNumber, String email, String password, UserRoles role, Double transactionLimit, Double dailyLimit) {
 
         this.firstName = firstName;
         this.lastName = lastName;
@@ -33,6 +37,8 @@ public class UserModel {
         this.email = email;
         this.password = password;
         this.role = role;
+        this.transactionLimit = transactionLimit;
+        this.dailyLimit = dailyLimit;
     }
 
     public String getFirstName() {
@@ -90,6 +96,23 @@ public class UserModel {
     public void setRole(UserRoles roles) {
         this.role = role;
 
+    }
+//TODO: change to actual daily and transaction limits
+    //TODO: the logic - for example transaction limit cant be higher than daily limit
+    public void setDailyLimit(double dailyLimit) {
+        this.dailyLimit = 100;
+    }
+
+    public void setTransactionLimit(double transactionLimit) {
+        this.transactionLimit = 50;
+    }
+
+    public double getDailyLimit() {
+        return dailyLimit;
+    }
+
+    public double getTransactionLimit() {
+        return transactionLimit;
     }
 }
 
