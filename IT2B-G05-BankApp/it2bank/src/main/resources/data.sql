e68ec110ad8e3743cd40095ad2f1dd6fd1d25eb3
RobyParak <677676@student.inholland.nl>
2023-06-01 14:14:41 +0200

    Added user to test

--- IT2B-G05-BankApp/it2bank/src/main/resources/data.sql
+++ IT2B-G05-BankApp/it2bank/src/main/resources/data.sql
@@ -3,4 +3,4 @@ VALUES ('Soft', 'Kitty', 12345678, '0612345678', 'softest_kitty@gmail.com', 'Myv
 INSERT INTO user_model(first_name, last_name, bsn, phone_number, email, password, role)
 VALUES ('Revolver', 'Ocelot', 23456789, '0676934748', 'shalashaska_ocelot@gmail.com', 'liquidOcelot4', 0);
 INSERT INTO user_model(first_name, last_name, bsn, phone_number, email, password, role)
-VALUES ('Solid', 'Snake', 34567890, '0612345678', 'solidsnake@gmail.com', 'solidSnake4', 2);
+VALUES ('Solid', 'Snake', 34567890, '0612345678', 'solidsnake@gmail.com', 'solidSnake4', 1);
