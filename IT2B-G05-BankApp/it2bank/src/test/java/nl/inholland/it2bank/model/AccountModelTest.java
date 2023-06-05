c47ae6886c1acd8f660d7f2fd7d7d8b63a851f54
Catalin246 <avorniceseicatalin1234@gmail.com>
2023-06-05 13:53:11 +0200

    uni testing

--- IT2B-G05-BankApp/it2bank/src/test/java/nl/inholland/it2bank/model/AccountModelTest.java
+++ IT2B-G05-BankApp/it2bank/src/test/java/nl/inholland/it2bank/model/AccountModelTest.java
@@ -1,72 +1,7 @@
 package nl.inholland.it2bank.model;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountModelTest {
 
-    @Test
-    void getIban() {
-    }
-
-    @Test
-    void setIban() {
-    }
-
-    @Test
-    void getOwnerId() {
-    }
-
-    @Test
-    void setOwnerId() {
-    }
-
-    @Test
-    void getStatusId() {
-    }
-
-    @Test
-    void setStatusId() {
-    }
-
-    @Test
-    void getAmount() {
-    }
-
-    @Test
-    void setAmount() {
-    }
-
-    @Test
-    void getAbsoluteLimit() {
-    }
-
-    @Test
-    void setAbsoluteLimit() {
-    }
-
-    @Test
-    void getTypeId() {
-    }
-
-    @Test
-    void setTypeId() {
-    }
-
-    @Test
-    void testEquals() {
-    }
-
-    @Test
-    void canEqual() {
-    }
-
-    @Test
-    void testHashCode() {
-    }
-
-    @Test
-    void testToString() {
-    }
 }
