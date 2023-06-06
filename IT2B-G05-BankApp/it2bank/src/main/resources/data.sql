0136907ac6dbbabd3cd7aaf9fdf0bd5542f7b28a
RobyParak <677676@student.inholland.nl>
2023-06-06 11:45:07 +0200

    Sorted out bankaccount issue

--- IT2B-G05-BankApp/it2bank/src/main/resources/data.sql
+++ IT2B-G05-BankApp/it2bank/src/main/resources/data.sql
@@ -11,8 +11,10 @@ VALUES ('Bank', 'je', 00000000, '0612345678', 'bankje@gmail.com', '$2a$10$8JSYaq
 INSERT INTO account_model (iban, owner_id, status_id, amount, absolute_limit, type_id)
 VALUES ('NL01INHO0000000001', 4, 0, 10000000, 0, 1);
 INSERT INTO account_model (iban, owner_id, status_id, amount, absolute_limit, type_id)
-VALUES ('NL01INHO0000000014', 3, 0, 1000, 0, 2);
+VALUES ('NL01INHO0000000014', 3, 0, 1000, 0, 1);
 INSERT INTO account_model (iban, owner_id, status_id, amount, absolute_limit, type_id)
-VALUES ('NL01INHO0000000022', 1, 0, 1000, 0, 2);
+VALUES ('NL01INHO0000000022', 3, 0, 1000, 0, 0);
 INSERT INTO account_model (iban, owner_id, status_id, amount, absolute_limit, type_id)
-VALUES ('NL01INHO0000000030', 2, 0, 1000, 0, 2);
+VALUES ('NL01INHO0000000030', 2, 0, 1000, 0, 1);
+INSERT INTO account_model (iban, owner_id, status_id, amount, absolute_limit, type_id)
+VALUES ('NL01INHO0000000048', 4, 0, 1000, 0, 0);
