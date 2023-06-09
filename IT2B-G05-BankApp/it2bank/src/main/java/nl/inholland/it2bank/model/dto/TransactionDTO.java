79907591131793db5e5e488429a082246e5082eb
Catalin246 <avorniceseicatalin1234@gmail.com>
2023-06-09 23:12:27 +0200

    Update

--- IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/model/dto/TransactionDTO.java
+++ IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/model/dto/TransactionDTO.java
@@ -1,6 +1,8 @@
 package nl.inholland.it2bank.model.dto;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
-public record TransactionDTO(int userPerforming, String accountFrom, String accountTo, double amount, LocalTime time, String comment) {
+public record TransactionDTO(int userPerforming, String accountFrom, String accountTo, double amount, LocalDateTime time, String comment) {
 }
