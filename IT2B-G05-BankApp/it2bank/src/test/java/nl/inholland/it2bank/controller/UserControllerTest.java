b60fe5c7ccc0a006340bdeb2e7b453a9e9c353ef
Matudor7 <tdr.nsk@gmail.com>
2023-06-09 13:25:15 +0200

    added security test dependency, commented bank account data

--- IT2B-G05-BankApp/it2bank/src/test/java/nl/inholland/it2bank/controller/UserControllerTest.java
+++ IT2B-G05-BankApp/it2bank/src/test/java/nl/inholland/it2bank/controller/UserControllerTest.java
@@ -6,6 +6,7 @@ import nl.inholland.it2bank.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
