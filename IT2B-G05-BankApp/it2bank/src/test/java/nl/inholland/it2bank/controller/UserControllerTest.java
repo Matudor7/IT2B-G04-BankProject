45ea93835b7de6acb401ece927f23caca9762e15
Matudor7 <tdr.nsk@gmail.com>
2023-06-09 14:11:10 +0200

    added JW Token test recognition

--- IT2B-G05-BankApp/it2bank/src/test/java/nl/inholland/it2bank/controller/UserControllerTest.java
+++ IT2B-G05-BankApp/it2bank/src/test/java/nl/inholland/it2bank/controller/UserControllerTest.java
@@ -1,14 +1,21 @@
 package nl.inholland.it2bank.controller;
 
+import nl.inholland.it2bank.config.ApiTestConfiguration;
 import nl.inholland.it2bank.model.UserModel;
 import nl.inholland.it2bank.model.UserRoles;
 import nl.inholland.it2bank.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -16,10 +23,13 @@ import java.util.List;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.hasSize;
 
 @WebMvcTest(UserController.class)
+@Import(ApiTestConfiguration.class)
 public class UserControllerTest {
 
     @MockBean
@@ -28,6 +38,7 @@ public class UserControllerTest {
     MockMvc mockMvc;
 
     @Test
+    @WithMockUser
     void getShouldReturnAllUsers() throws Exception{
         List<UserModel> users;
 
@@ -36,7 +47,8 @@ public class UserControllerTest {
                         new UserModel("Firstname", "Lastname", 123345678L, "0612343567", "email@address.com", "Password", UserRoles.User, 50.0, 100.0)
                 ));
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/users")).andDo(MockMvcResultHandlers.print())
+        mockMvc.perform(MockMvcRequestBuilders.get("/users").contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(1)));
     }
