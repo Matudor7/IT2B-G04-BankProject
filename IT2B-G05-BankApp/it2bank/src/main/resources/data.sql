15c7fddb20446e8756641844ddabe41055cb87eb
RobyParak <677676@student.inholland.nl>
2023-06-10 11:39:18 +0200

    Commented in back the accounts. I need accounts to test.

--- IT2B-G05-BankApp/it2bank/src/main/resources/data.sql
+++ IT2B-G05-BankApp/it2bank/src/main/resources/data.sql
@@ -8,17 +8,17 @@ VALUES ('Solid', 'Snake', 34567890, '0612345678', 'solidsnake@gmail.com', '$2a$1
 -- This below is the bank itself bank account. DO NOT remove this.
 INSERT INTO user_model(first_name, last_name, bsn, phone_number, email, password, role, daily_limit, transaction_limit) -- the password for this account is "Bankje!" but hashed --
 VALUES ('Bank', 'je', 00000000, '0612345678', 'bankje@gmail.com', '$2a$10$8JSYaqGtWoZvaGhkbrI5/eUxwtlIHrmcDlJWgYlq5CMjVtSMwkBdG', 1, 100, 50);
---INSERT INTO bank_account_model (iban, owner_id, status_id, balance, absolute_limit, type_id)
---VALUES ('NL01INHO0000000001', 4, 0, 10000000, 0, 1);
---INSERT INTO bank_account_model (iban, owner_id, status_id, balance, absolute_limit, type_id)
---VALUES ('NL01INHO0000000014', 3, 0, 1000, 0, 1);
---INSERT INTO bank_account_model (iban, owner_id, status_id, balance, absolute_limit, type_id)
---VALUES ('NL01INHO0000000022', 3, 0, 1000, 0, 0);
---INSERT INTO bank_account_model (iban, owner_id, status_id, balance, absolute_limit, type_id)
---VALUES ('NL01INHO0000000030', 2, 0, 1000, 0, 1);
---INSERT INTO bank_account_model (iban, owner_id, status_id, balance, absolute_limit, type_id)
---VALUES ('NL01INHO0000000048', 4, 0, 1000, 0, 0);
---INSERT INTO transaction_model (user_performing, account_from, account_to, amount, time, comment)
---VALUES (2, 'NLHO234', 'NLHOIO23456', '1234', '10:04', '');
---INSERT INTO transaction_model (user_performing, account_from, account_to, amount, time, comment)
---VALUES (3, 'NL01INHO0000000001', 'NLHOIO23456', '1234', '10:04', 'test');
+INSERT INTO bank_account_model (iban, owner_id, status_id, balance, absolute_limit, type_id)
+VALUES ('NL01INHO0000000001', 4, 0, 10000000, 0, 1);
+INSERT INTO bank_account_model (iban, owner_id, status_id, balance, absolute_limit, type_id)
+VALUES ('NL01INHO0000000014', 3, 0, 1000, 0, 1);
+INSERT INTO bank_account_model (iban, owner_id, status_id, balance, absolute_limit, type_id)
+VALUES ('NL01INHO0000000022', 3, 0, 1000, 0, 0);
+INSERT INTO bank_account_model (iban, owner_id, status_id, balance, absolute_limit, type_id)
+VALUES ('NL01INHO0000000030', 2, 0, 1000, 0, 1);
+INSERT INTO bank_account_model (iban, owner_id, status_id, balance, absolute_limit, type_id)
+VALUES ('NL01INHO0000000048', 4, 0, 1000, 0, 0);
+-- INSERT INTO transaction_model (user_performing, account_from, account_to, amount, time, comment)
+-- VALUES (2, 'NLHO234', 'NLHOIO23456', '1234', '10:04', '');
+-- INSERT INTO transaction_model (user_performing, account_from, account_to, amount, time, comment)
+-- VALUES (3, 'NL01INHO0000000001', 'NLHOIO23456', '1234', '10:04', 'test');
