a89584b6ee9c0ef614b52f42f04fbde8904217d0
Catalin246 <avorniceseicatalin1234@gmail.com>
2023-06-12 16:30:55 +0200

    testing

--- IT2B-G05-BankApp/it2bank/src/test/java/nl/inholland/it2bank/controller/TransactionControllerTest.java
+++ IT2B-G05-BankApp/it2bank/src/test/java/nl/inholland/it2bank/controller/TransactionControllerTest.java
@@ -1,4 +1,21 @@
 package nl.inholland.it2bank.controller;
 
+import nl.inholland.it2bank.config.ApiTestConfiguration;
+import nl.inholland.it2bank.service.TransactionService;
+import nl.inholland.it2bank.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(TransactionController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@Import(ApiTestConfiguration.class)
 public class TransactionControllerTest {
+    @MockBean
+    TransactionService transactionService;
+    @Autowired
+    MockMvc mockMvc;
 }
