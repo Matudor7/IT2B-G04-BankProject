fbd5ddc4a76689a33e04761c8789bb048ea6ac75
Tudor <tdr.nsk@gmail.com>
2023-06-12 16:49:38 +0200

    Update BankAccountController.java

--- IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/controller/BankAccountController.java
+++ IT2B-G05-BankApp/it2bank/src/main/java/nl/inholland/it2bank/controller/BankAccountController.java
@@ -9,6 +9,8 @@ import nl.inholland.it2bank.model.*;
 import nl.inholland.it2bank.model.dto.BankAccountDTO;
 import nl.inholland.it2bank.service.BankAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +23,7 @@ import java.util.Optional;
 @Log
 public class BankAccountController {
 
+    @Autowired
     private final BankAccountService bankAccountService;
 
     public BankAccountController(BankAccountService bankAccountService) {
@@ -40,11 +43,22 @@ public class BankAccountController {
             @RequestParam(value = "statusId", required = false) Integer statusId,
             @RequestParam(value = "balance", required = false) Double balance,
             @RequestParam(value = "absoluteLimit", required = false) Integer absoluteLimit,
-            @RequestParam(value = "typeId", required = false) Integer typeId
+            @RequestParam(value = "typeId", required = false) Integer typeId,
+            @RequestParam(value = "first_name", required = false) String firstName,
+            @RequestParam(value = "last_name", required = false) String lastName
     ) {
-        List<BankAccountModel> accounts = bankAccountService.findAccountByAttributes(iban, ownerId, statusId, balance, absoluteLimit, typeId);
+        List<BankAccountModel> accounts;
+        if (firstName != null) {
+            accounts = bankAccountService.findAccountByFirstName(firstName);
+        } else if (lastName != null) {
+            accounts = bankAccountService.findAccountByLastName(lastName);
+        }
+        else {
+            accounts = bankAccountService.findAccountByAttributes(iban, ownerId, statusId, balance, absoluteLimit, typeId);
+        }
         return ResponseEntity.ok(accounts);
     }
+
 
     @PostMapping
     @ApiOperation(value = "Create Account", notes = "Successfully created account")
@@ -59,13 +73,39 @@ public class BankAccountController {
     }
 
     @PutMapping("{iban}")
-    @ApiOperation(value = "Update account", notes = "Update an account based on the iban")
+    @ApiOperation(value = "Get accounts", notes = "Retrieve accounts based on filters")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successfully updated account", response = BankAccountModel.class, responseContainer = "List"),
+            @ApiResponse(code = 200, message = "Successfully retrieved accounts", response = BankAccountModel.class, responseContainer = "List"),
             @ApiResponse(code = 400, message = "Malformed request syntax")
     })
-    public ResponseEntity<Object> updateBankAccountById(@PathVariable String iban, @RequestBody BankAccountDTO bankAccountDto) {
-        bankAccountService.updateBankAccount(iban, bankAccountDto);
-        return ResponseEntity.ok().body(bankAccountDto);
+
+    public ResponseEntity<Object> updateBankAccountByIban(@PathVariable String iban, @RequestBody BankAccountDTO bankAccountDto) {
+        Optional<BankAccountModel> existingAccount = bankAccountService.getAccountByIban(iban);
+
+        if (existingAccount.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+//        BankAccountModel account = existingAccount.get();
+//
+//        if (bankAccountDto.ownerId() != 0) {
+//            account.setOwnerId(bankAccountDto.ownerId());
+//        }
+//        if (bankAccountDto.statusId() != null) {
+//            account.setStatusId(bankAccountDto.statusId());
+//        }
+//        if (bankAccountDto.balance() != 0) {
+//            account.setBalance(bankAccountDto.balance());
+//        }
+//        if (bankAccountDto.absoluteLimit() != 0) {
+//            account.setAbsoluteLimit(bankAccountDto.absoluteLimit());
+//        }
+//        if (bankAccountDto.typeId() != null) {
+//            account.setTypeId(bankAccountDto.typeId());
+//        }
+
+        BankAccountModel updatedAccount = bankAccountService.saveAccount(bankAccountDto);
+
+        return ResponseEntity.ok(updatedAccount);
     }
 }
