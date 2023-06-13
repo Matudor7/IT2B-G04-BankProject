ca286657eae4a904b5808671f01bf64d6e69544c
myrthe678105 <90244561+myrthe678105@users.noreply.github.com>
2023-06-13 12:58:36 +0200

    more transactions

--- IT2B-G05-BankApp/it2bank/src/main/resources/data.sql
+++ IT2B-G05-BankApp/it2bank/src/main/resources/data.sql
@@ -30,3 +30,32 @@ INSERT INTO transaction_model (user_performing, account_from, account_to, amount
 VALUES (2, 'NLHO234', 'NLHOIO23456', '1234', '2023-06-12T10:04:00', '');
 INSERT INTO transaction_model (user_performing, account_from, account_to, amount, date_time, comment)
 VALUES (3, 'NL01INHO0000000001', 'NLHOIO23456', '1234', '2023-06-12T10:04:00', 'test');
+INSERT INTO transaction_model (user_performing, account_from, account_to, amount, date_time, comment)
+VALUES (2, 'NLHO234', 'NLHOIO23456', '500', '2023-06-13T09:15:00', 'Payment for rent');
+
+INSERT INTO transaction_model (user_performing, account_from, account_to, amount, date_time, comment)
+VALUES (3, 'NL01INHO0000000001', 'NLHOIO23456', '300', '2023-06-13T12:30:00', 'Grocery shopping');
+
+INSERT INTO transaction_model (user_performing, account_from, account_to, amount, date_time, comment)
+VALUES (4, 'NL01INHO0000000014', 'NLHO234', '150', '2023-06-14T14:45:00', 'Dinner with friends');
+
+INSERT INTO transaction_model (user_performing, account_from, account_to, amount, date_time, comment)
+VALUES (2, 'NL01INHO0000000030', 'NL01INHO0000000048', '1000', '2023-06-14T16:30:00', 'Loan repayment');
+
+INSERT INTO transaction_model (user_performing, account_from, account_to, amount, date_time, comment)
+VALUES (3, 'NLHOIO23456', 'NL01INHO0000000022', '200', '2023-06-15T10:00:00', 'Clothing purchase');
+
+INSERT INTO transaction_model (user_performing, account_from, account_to, amount, date_time, comment)
+VALUES (4, 'NL01INHO0000000048', 'NLHO234', '800', '2023-06-16T13:15:00', 'Travel expenses');
+
+INSERT INTO transaction_model (user_performing, account_from, account_to, amount, date_time, comment)
+VALUES (2, 'NL01INHO0000000030', 'NLHOIO23456', '600', '2023-06-17T16:45:00', 'Payment for services');
+
+INSERT INTO transaction_model (user_performing, account_from, account_to, amount, date_time, comment)
+VALUES (3, 'NLHOIO23456', 'NL01INHO0000000022', '150', '2023-06-18T11:30:00', 'Coffee with colleagues');
+
+INSERT INTO transaction_model (user_performing, account_from, account_to, amount, date_time, comment)
+VALUES (4, 'NL01INHO0000000014', 'NLHO234', '400', '2023-06-18T14:00:00', 'Gift purchase');
+
+INSERT INTO transaction_model (user_performing, account_from, account_to, amount, date_time, comment)
+VALUES (2, 'NL01INHO0000000001', 'NL01INHO0000000048', '250', '2023-06-19T09:30:00', 'Transfer to savings account');
